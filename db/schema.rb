12b4944b9fd44f1802dac26e34ec0c9ba7222b06
Ollie Bennett <olliebennett@gmail.com>
2016-09-07 23:05:26 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907211124) do
+ActiveRecord::Schema.define(version: 20160907213858) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160907211124) do
     t.integer  "price"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "title"
+    t.string   "address"
   end
 
 end
