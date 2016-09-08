012201f0c54e41f42a7bdeb3278db18a71939767
Ollie Bennett <olliebennett@gmail.com>
2016-09-08 08:19:17 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907213858) do
+ActiveRecord::Schema.define(version: 20160908071527) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
     t.integer  "home_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["home_id"], name: "index_comments_on_home_id"
+    t.index ["home_id"], name: "index_comments_on_home_id", using: :btree
   end
 
   create_table "homes", force: :cascade do |t|
@@ -29,6 +32,23 @@ ActiveRecord::Schema.define(version: 20160907213858) do
     t.datetime "updated_at",    null: false
     t.string   "title"
     t.string   "address"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
