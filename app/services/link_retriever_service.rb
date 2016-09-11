754e634884f0d002e7ad41332754b8160e02a442
Ollie Bennett <olliebennett@gmail.com>
2016-09-11 09:03:02 +0100

    Improve and test zoopla data importer

--- app/services/link_retriever_service.rb
+++ app/services/link_retriever_service.rb
@@ -3,30 +3,28 @@ require 'open-uri'
 
 class LinkRetrieverService
   def self.retrieve(url)
-    puts "Retrieving link: #{url}"
-    page = Nokogiri::HTML(open(url))
+    file = open(url)
+
+    parse_zoopla(file)
+  end
+
+  def self.parse_zoopla(file)
+    page = Nokogiri::HTML(file)
 
     data = {}
-    # puts "Retrieved data: #{page}"
-    data[:canonical_url] = page.xpath("//meta[@property='og:url']/@content")
-    puts "canonical_url: #{data[:canonical_url]}"
+    data[:canonical_url] = page.xpath("//link[@rel='canonical']/@href").try(:to_s)
 
-    data[:address] = page.xpath("//meta[@property='og:street-address']/@content")
-    puts "address: #{data[:address]}"
-    data[:postcode] = page.xpath("//meta[@property='og:postal-code']/@content")
-    puts "postcode: #{data[:postcode]}"
+    data[:address] = page.xpath("//h2[@itemprop='streetAddress']").try(:text).try(:to_s)
 
-    data[:longitude] = page.xpath("//meta[@property='og:longitude']/@content")
-    puts "longitude: #{data[:longitude]}"
-    data[:latitude] = page.xpath("//meta[@property='og:latitude']/@content")
-    puts "latitude: #{data[:latitude]}"
+    data[:postcode] = page.xpath("//meta[@property='og:postal-code']/@content").try(:to_s)
 
-    data[:description] = page.xpath("//meta[@property='og:description']/@content").to_s.squish
-    puts "description: #{data[:description]}"
+    data[:longitude] = page.xpath("//meta[@itemprop='longitude']/@content").try(:to_s).try(:to_f)
+    data[:latitude] = page.xpath("//meta[@itemprop='latitude']/@content").try(:to_s).try(:to_f)
 
-    data[:price] = data[:description].match(/[\d\,\.]{5,}/)
-    data[:price] = data[:price].to_s.tr(',','') if data[:price].present?
-    puts "price: #{data[:price]}"
+    data[:description] = page.xpath("//div[@itemprop='description']").try(:text).try(:to_s).try(:squish)
+
+    data[:price] = page.css('.listing-details-price.text-price')[0].text.squish
+    data[:price] = data[:price].match(/[\d\,\.]{5,}/)[0].tr(',','').to_i
 
     data[:images] = page.xpath("//meta[@property='og:image']/@content").map(&:to_s)
 
