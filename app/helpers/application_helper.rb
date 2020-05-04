f0756696025644c019ebc60f61607a75a73abc6e
Ollie Bennett <olliebennett@gmail.com>
2020-05-04 07:46:40 +0100

    Improve UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_markdown_paragraphs(text_with_newlines)
+    capture do
+      text_with_newlines.split("\n").reject(&:blank?).each do |line|
+        concat content_tag(:p, line)
+      end
+    end
+  end
 end
