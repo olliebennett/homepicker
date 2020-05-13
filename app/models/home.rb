d08063ce55952a58a72140dfbd6ae45262537be2
Ollie Bennett <olliebennett@gmail.com>
2020-05-13 06:55:26 +0100

    Replace table UI on homes list

--- app/models/home.rb
+++ app/models/home.rb
@@ -7,6 +7,14 @@ class Home < ApplicationRecord
 
   accepts_nested_attributes_for :images, reject_if: :mark_empty_images_for_destruction, allow_destroy: true
 
+  def default_image
+    images.first&.url || placeholder_image
+  end
+
+  def placeholder_image
+    'https://homepicker.s3.eu-west-2.amazonaws.com/homepicker.svg'
+  end
+
   def mark_empty_images_for_destruction(attributes)
     exists = attributes['id'].present?
     empty = attributes.except(:id).values.all?(&:blank?)
