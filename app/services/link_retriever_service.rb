b6e9a8976a38207392ea9b4c1760d223f1ff53f6
Ollie Bennett <olliebennett@gmail.com>
2020-05-13 06:56:22 +0100

    Fix support for .co.uk Zoopla links

--- app/services/link_retriever_service.rb
+++ app/services/link_retriever_service.rb
@@ -5,7 +5,7 @@ class LinkRetrieverService
   def self.retrieve(url)
     page_html = URI.open(url) { |f| f.read }
 
-    if url.include?('zoopla.com')
+    if url.include?('zoopla.co') # .com or .co.uk both supported!
       parse_zoopla(page_html)
     elsif url.include?('rightmove.co.uk')
       parse_rightmove(page_html)
