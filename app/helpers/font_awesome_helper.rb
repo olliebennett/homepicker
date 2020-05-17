447ff00fadcb0af3ab19e291202487299d083bff
Ollie Bennett <olliebennett@gmail.com>
2020-05-17 19:17:06 +0100

    Improve UI and tooltip text for collaborative ratings

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -1,5 +1,17 @@
 module FontAwesomeHelper
-  def fas_icon(glyph, text: nil)
-    content_tag(:i, text, class: "fas fa-#{glyph}")
+  def fa_icon(glyph, type: :solid, text: nil)
+    fa_prefix = case type
+                when :solid
+                  'fas'
+                when :light
+                  'fal'
+                when :regular
+                  'far'
+                when :duotone
+                  'fad'
+                else
+                  raise "Unexpected FontAwesome type; #{type}; expected regular/solid/light/duotone"
+                end
+    content_tag(:i, text, class: "#{fa_prefix} fa-#{glyph}")
   end
 end
