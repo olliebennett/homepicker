43883bdddeed037959c4f1e847c3ccceb1b67c42
Ollie Bennett <olliebennett@gmail.com>
2020-09-07 00:29:37 +0100

    Create job to upload image to S3 with thumb

---
+++ app/jobs/image_persist_job.rb
@@ -0,0 +1,54 @@
+require 'transloadit'
+
+class ImagePersistJob < ApplicationJob
+  def perform(image_id)
+    image = Image.find!(image_id)
+
+    transloadit = Transloadit.new(
+      key: ENV.fetch('TRANSLOADIT_KEY'),
+      secret: ENV.fetch('TRANSLOADIT_SECRET')
+    )
+
+    steps = []
+    steps << transloadit.step('original',
+                              '/http/import',
+                              url: image.url)
+    steps << transloadit.step('filter',
+                              '/file/filter',
+                              use: 'original',
+                              accepts: [['${file.mime}', 'regex', 'image']],
+                              error_on_decline: true)
+    steps << transloadit.step('thumb',
+                              '/image/resize',
+                              use: 'filter',
+                              width: 150,
+                              height: 150,
+                              imagemagick_stack: 'v2.0.7',
+                              resize_strategy: 'fillcrop')
+    steps << transloadit.step('store',
+                              '/s3/store',
+                              use: %w[thumb original],
+                              credentials: 'AWS_S3_CREDENTIALS')
+
+    assembly = transloadit.assembly(steps: steps)
+
+    # Empty array as no 'files' needed; we set the image URL above instead
+    response = assembly.create!([])
+
+    # reloads the response once per second until all processing is finished
+    response.reload_until_finished!
+
+    if response.error?
+      raise 'assembly errored!'
+    else
+      results = response.body.fetch('results')
+      original = results.fetch('original').fetch(0).fetch('ssl_url')
+      thumb = results.fetch('thumb').fetch(0).fetch('ssl_url')
+
+      # TODO: Persist 'original' and 'thumb' URLs to Image record
+      puts 'Uploaded files;'
+      puts "Original: #{original}"
+      puts "Thumb: #{thumb}"
+    end
+  end
+end
