f1fc83b04e030b8c6d1e3b10d257602cad6d9300
Ollie Bennett <olliebennett@gmail.com>
2020-09-07 00:42:04 +0100

    Schema for renamed external_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_222828) do
+ActiveRecord::Schema.define(version: 2020_09_06_233256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_222828) do
   end
 
   create_table "images", id: :serial, force: :cascade do |t|
-    t.string "url", null: false
+    t.string "external_url", null: false
     t.integer "home_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
