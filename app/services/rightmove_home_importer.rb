69caa7a5c5248b942d56ed3caf2d0770e28460b6
Ollie Bennett <olliebennett@gmail.com>
2020-09-26 15:01:17 +0100

    Rebuild Rightmove worker to handle site redesign

--- app/services/rightmove_home_importer.rb
+++ app/services/rightmove_home_importer.rb
@@ -2,36 +2,32 @@ class RightmoveHomeImporter < HomeImporter
   def self.parse(page_html)
     page = Nokogiri::HTML(page_html)
 
-    puts "RIGHTMOVE: starting..."
-
     data = {}
 
-    property_json_string = page_html.match(/'property',{"location":{(.*)}/)
-    property_json = JSON.parse(property_json_string[0][11..-1])
+    data[:canonical_url] = page.xpath("//meta[@property='og:url']/@content")&.to_s
 
-    data[:postcode] = property_json.dig('location', 'postcode')
-    raise "RIGHTMOVE: cannot find postcode..." if data[:postcode].blank?
-    data[:latitude] = property_json.dig('location', 'latitude')
-    data[:longitude] = property_json.dig('location', 'longitude')
+    property_json_string = page_html.match(/window\.PAGE_MODEL = {(.*)}/)
 
-    data[:price] = property_json.dig('propertyInfo', 'price')
-    data[:price] = 0 if data[:price] == nil
+    property_json = JSON.parse("{#{property_json_string[1]}}") if property_json_string.present?
 
-    data[:title] = page.css('div.property-header-bedroom-and-price/div/h1')[0].text
-    raise "RIGHTMOVE: cannot find title..." if data[:title].blank?
-    data[:title] = data[:title].gsub('for sale', '').squish
+    if property_json.present?
+      data[:postcode] = property_json.dig('analyticsInfo', 'analyticsProperty', 'postcode')
+      data[:latitude] = property_json.dig('analyticsInfo', 'analyticsProperty', 'latitude')
+      data[:longitude] = property_json.dig('analyticsInfo', 'analyticsProperty', 'longitude')
+      data[:price] = property_json.dig('analyticsInfo', 'analyticsProperty', 'price')
+      data[:price] = 0 if data[:price].blank?
 
-    data[:description] = html_to_markdown(page.xpath("//p[@itemprop='description']")[0])
+      data[:title] = property_json.dig('propertyData', 'text', 'pageTitle')
+      data[:description] = property_json.dig('propertyData', 'text', 'description')
 
-    data[:canonical_url] = page.xpath("//link[@rel='canonical']/@href")&.to_s
-
-    address = page.css("div.property-header-bedroom-and-price/div/address/meta[@itemprop='streetAddress']/@content")[0].text
-    raise "RIGHTMOVE: cannot find address..." if address.blank?
-
-    data[:address_street], data[:address_locality] = address.split(',', 2)
-    raise "RIGHTMOVE: cannot find address_locality..." if data[:address_locality].blank?
-
-    data[:address_region] = 'UNKNOWN'
+      address = property_json.dig('propertyData', 'address', 'displayAddress')
+      if address.present?
+        address_parts = address.split(',', 3)
+        data[:address_street] = address_parts[0]
+        data[:address_locality] = address_parts[1]
+        data[:address_region] = address_parts[2] || 'UNKNOWN'
+      end
+    end
 
     data[:images] = page.xpath("//meta[@property='og:image']/@content").map(&:to_s)
 
