8cd48ca65ff1be7345eecdd22a31e1a2c43eb86f
Ollie Bennett <olliebennett@gmail.com>
2020-10-19 22:22:31 +0100

    Refactor home params

--- app/services/home_params.rb
+++ app/services/home_params.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 class HomeParams
+  PERMITTED_PARAMS = %i[
+    title
+    description
+    address_street
+    address_locality
+    address_region
+    postcode
+    latitude
+    longitude
+    agent_url
+    zoopla_url
+    rightmove_url
+    price
+    hunt_id
+  ].freeze
+
   def initialize(params)
     @params = params
   end
 
   def permitted
     @params.require(:home).permit(
-      :title,
-      :description,
-      :address_street,
-      :address_locality,
-      :address_region,
-      :postcode,
-      :latitude,
-      :longitude,
-      :agent_url,
-      :zoopla_url,
-      :rightmove_url,
-      :price,
-      :hunt_id,
+      PERMITTED_PARAMS,
       images_attributes: %i[id external_url _destroy]
     )
   end
