a171dd7af4bd41a9dfbc91657d5b7e51d213e2f9
Ollie Bennett <olliebennett@gmail.com>
2020-10-19 08:16:17 +0100

    Refactor image persist job

--- app/jobs/image_persist_job.rb
+++ app/jobs/image_persist_job.rb
@@ -4,51 +4,85 @@ require 'transloadit'
 
 class ImagePersistJob < ApplicationJob
   def perform(image_id)
-    image = Image.find(image_id)
+    @image = Image.find(image_id)
 
-    transloadit = Transloadit.new(
+    await_response
+
+    if @response.error?
+      raise 'assembly errored!'
+    else
+      update_image
+    end
+  end
+
+  private
+
+  def assembly
+    transloadit.assembly(steps: steps)
+  end
+
+  def await_response
+    # Empty array as no 'files' needed; we set the image URL above instead
+    @response = assembly.create!([])
+
+    # reloads the response once per second until all processing is finished
+    @response.reload_until_finished!
+  end
+
+  def steps
+    [
+      step_original,
+      step_filter,
+      step_thumb,
+      step_store
+    ]
+  end
+
+  def step_original
+    transloadit.step('original',
+                     '/http/import',
+                     url: @image.external_url)
+  end
+
+  def step_filter
+    transloadit.step('filter',
+                     '/file/filter',
+                     use: 'original',
+                     accepts: [['${file.mime}', 'regex', 'image']],
+                     error_on_decline: true)
+  end
+
+  def step_thumb
+    transloadit.step('thumb',
+                     '/image/resize',
+                     use: 'filter',
+                     width: 150,
+                     height: 150,
+                     imagemagick_stack: 'v2.0.7',
+                     resize_strategy: 'fillcrop')
+  end
+
+  def step_store
+    transloadit.step('store',
+                     '/s3/store',
+                     use: %w[thumb original],
+                     credentials: 'AWS_S3_CREDENTIALS')
+  end
+
+  def transloadit
+    @transloadit ||= Transloadit.new(
       key: ENV.fetch('TRANSLOADIT_KEY'),
       secret: ENV.fetch('TRANSLOADIT_SECRET')
     )
+  end
 
-    steps = []
-    steps << transloadit.step('original',
-                              '/http/import',
-                              url: image.external_url)
-    steps << transloadit.step('filter',
-                              '/file/filter',
-                              use: 'original',
-                              accepts: [['${file.mime}', 'regex', 'image']],
-                              error_on_decline: true)
-    steps << transloadit.step('thumb',
-                              '/image/resize',
-                              use: 'filter',
-                              width: 150,
-                              height: 150,
-                              imagemagick_stack: 'v2.0.7',
-                              resize_strategy: 'fillcrop')
-    steps << transloadit.step('store',
-                              '/s3/store',
-                              use: %w[thumb original],
-                              credentials: 'AWS_S3_CREDENTIALS')
+  def update_image
+    results = @response.body.fetch('results')
 
-    assembly = transloadit.assembly(steps: steps)
+    original = results.fetch('original').fetch(0).fetch('ssl_url')
+    thumb = results.fetch('thumb').fetch(0).fetch('ssl_url')
 
-    # Empty array as no 'files' needed; we set the image URL above instead
-    response = assembly.create!([])
-
-    # reloads the response once per second until all processing is finished
-    response.reload_until_finished!
-
-    if response.error?
-      raise 'assembly errored!'
-    else
-      results = response.body.fetch('results')
-      original = results.fetch('original').fetch(0).fetch('ssl_url')
-      thumb = results.fetch('thumb').fetch(0).fetch('ssl_url')
-
-      # Persist 'original' and 'thumb' URLs to Image record
-      image.update!(full_url: original, thumb_url: thumb)
-    end
+    # Persist 'original' and 'thumb' URLs to Image record
+    @image.update!(full_url: original, thumb_url: thumb)
   end
 end
