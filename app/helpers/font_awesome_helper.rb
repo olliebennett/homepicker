b1729099959199892eda7b63bfc5618077374c17
Ollie Bennett <olliebennett@gmail.com>
2020-10-19 22:26:33 +0100

    RuboCop: Rails/OutputSafety disabling

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -7,7 +7,11 @@ module FontAwesomeHelper
   def fa_icon_inline(glyph, type: :solid, cls: '')
     x = fa_icon_svg(glyph, type).dup
     x.gsub!('class="', "class=\"#{cls} ") if cls.present? # add custom classes
+    # We're confident it's safe because we define the <svg> data in the YAML config ourselves
+    # (we could parse/re-build the SVG safely, then modify class, but this would be unnecessary complexity)
+    # rubocop:disable Rails/OutputSafety
     x.html_safe
+    # rubocop:enable Rails/OutputSafety
   end
 
   private
