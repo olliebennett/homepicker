440763856b3579452b35137fb5745ec0e25a5904
Ollie Bennett <olliebennett@gmail.com>
2020-10-29 23:02:48 +0000

    Avoid public helper methods on all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
+  private
+
   # Devise: Path for sending users to after they log in or register
   def after_sign_in_path_for(_resource)
     stored_location_for(:user) || hunts_path
