19a1dcf82831da5313f9732f7b66c8e0a23ee2eb
Ollie Bennett <olliebennett@gmail.com>
2021-01-16 13:40:40 +0000

    Support adding home images in bulk

--- app/services/home_params.rb
+++ app/services/home_params.rb
@@ -22,9 +22,33 @@ class HomeParams
   end
 
   def permitted
+    bulk_images = @params[:home]&.delete(:bulk_images)
+    handle_bulk_images(bulk_images) if bulk_images.present?
+
     @params.require(:home).permit(
       PERMITTED_PARAMS,
       images_attributes: %i[id external_url _destroy]
     )
+  end
+
+  def handle_bulk_images(bulk_images)
+    # Split 'bulk images' textarea to separate fields
+    images = bulk_images.split("\n").map(&:squish).reject(&:blank?).uniq
+
+    images = deduplicate_images(images)
+
+    images.each do |image_url|
+      # Add the image to the hash (with sequential numeric keys)
+      next_key = @params[:home][:images_attributes].keys.count.to_s
+      @params[:home][:images_attributes][next_key] = { external_url: image_url }
+    end
+  end
+
+  def deduplicate_images(images)
+    @params[:home][:images_attributes] ||= []
+
+    images.reject do |image_url|
+      @params[:home][:images_attributes].values.any? { |v| v[:external_url] == image_url }
+    end
   end
 end
