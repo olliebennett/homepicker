5a94a7bc2273b17bbc4a692f1aa9237d46167c14
Ollie Bennett <olliebennett@gmail.com>
2021-02-15 17:27:45 +0000

    Use only HTML content for SparkPost emails

--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -4,5 +4,6 @@ SparkPostRails.configure do |c|
   c.api_key = ENV.fetch('SPARKPOST_API_KEY', '')
   c.api_endpoint = 'https://api.eu.sparkpost.com/api/'
   c.transactional = true
+  c.html_content_only = true
   c.subaccount = ENV.fetch('SPARKPOST_SUBACCOUNT_ID', nil)
 end
