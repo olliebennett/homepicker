4b010646ff92a157deacdb6bf63b5ec0960c64d9
Ollie Bennett <olliebennett@gmail.com>
2022-09-18 08:43:17 +0100

    Remove legacy Rightmove example

--- spec/services/rightmove_home_importer_spec.rb
+++ spec/services/rightmove_home_importer_spec.rb
@@ -2,65 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe RightmoveHomeImporter do
+RSpec.fdescribe RightmoveHomeImporter do
   describe '#parse' do
-    context 'with example 1' do
-      let(:rm1_file) { File.read('spec/fixtures/rightmove/example_1.html') }
-      let(:rm1) { described_class.new(rm1_file).parse }
-
-      it 'parses canonical url' do
-        expect(rm1[:rightmove_url]).to eq 'https://www.rightmove.co.uk/properties/83401585'
-      end
-
-      it 'parses address fields' do
-        expect(rm1).to include(
-          address_street: 'South Bank Tower',
-          address_locality: '55 Upper Ground',
-          address_region: 'Bankside'
-        )
-      end
-
-      it 'parses postcode' do
-        expect(rm1[:postcode]).to eq 'SE1 9RB'
-      end
-
-      it 'parses location data' do
-        expect(rm1).to include(
-          latitude: 51.507649,
-          longitude: -0.107771
-        )
-      end
-
-      it 'parses title' do
-        expect(rm1[:title]).to eq '2 bedroom apartment for sale'
-      end
-
-      it 'parses description' do
-        expect(rm1[:description]).to include 'a 20m indoor swimming pool'
-      end
-
-      it 'parses key features (and prepends to desc in list format)' do
-        expect(rm1[:description]).to include '- example key feature two'
-      end
-
-      it 'parses price' do
-        expect(rm1[:price]).to eq 1_875_000
-      end
-
-      it 'parses image data' do
-        expect(rm1[:images]).to include 'https://media.rightmove.co.uk/121k/120835/83401585/120835_SNE190293_IMG_21_0000.jpg'
-      end
-
-      it 'parses floorplan image url' do
-        # NOTE: floorplans are simply stored (last) in the images array
-        expect(rm1[:images].last).to eq 'https://media.rightmove.co.uk/121k/120835/83401585/120835_SNE190293_FLP_01_0000.jpg'
-      end
-
-      it 'ignores floorplan links' do
-        expect(rm1[:images]).not_to include 'https://www.example.com/floorplan_link'
-      end
-    end
-
     context 'with example 2' do
       let(:rm2_file) { File.read('spec/fixtures/rightmove/example_2.html') }
       let(:rm2) { described_class.new(rm2_file).parse }
