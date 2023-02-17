cf2b5f21eb1d174b7e46da03bbbad1713ed121d8
Ollie Bennett <olliebennett@gmail.com>
2023-02-17 07:08:39 +0000

    Workaround for renamed REDIS_URL env var

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-redis_uri = ENV.fetch('REDIS_URL')
+# Workaround to use externally managed ENV var by fetching the specified config key instead
+redis_uri = ENV.fetch('REDIS_URL') { |_| ENV.fetch(ENV.fetch('REDIS_URL_ENV_KEY')) }
 REDIS = Redis.new(url: redis_uri)
 
 Resque.redis = REDIS
