8409b7354e108eaf5e6882513b9f2cf21431c78d
Ollie Bennett <olliebennett@gmail.com>
2024-06-18 22:49:13 +0100

    Refactor markdown rendering

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module CommentsHelper
+  def markdown(text)
+    redcarpet.render(text).html_safe
+  end
+
+  private
+
+  def redcarpet
+    @redcarpet ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+  end
 end
